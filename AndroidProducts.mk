9c5a042ad2269d96a6dd07f5d5bc7a8d681da5b2
NexusBR-odessa <miguel03barreto@gmail.com>
2025-09-01 16:56:46 -0300

    YAAP Bring Up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_odessa.mk
+    $(LOCAL_DIR)/yaap_odessa.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_odessa-bp2a-eng \
-    lineage_odessa-bp2a-user \
-    lineage_odessa-bp2a-userdebug
+    yaap_odessa-bp2a-eng \
+    yaap_odessa-bp2a-user \
+    yaap_odessa-bp2a-userdebug
--- yaap_odessa.mk
+++ yaap_odessa.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/odessa/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Flags
 TARGET_DISABLE_EPPE := true
@@ -32,7 +32,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_BLUR := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_odessa
+PRODUCT_NAME := yaap_odessa
 PRODUCT_DEVICE := odessa
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(9) plus
